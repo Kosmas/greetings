31e22c43616216f2698114b96f851c83ff6dfcc2
Kosmas Chatzimichalis <kosmas@mach7x.com>
2018-02-18 13:43:19 +0100

    Add dynamic endpoints, /, name, an everyting else

--- lib/greetings.ex
+++ lib/greetings.ex
@@ -1,9 +1,27 @@
 defmodule Greetings do
-  use Ace.HTTP.Service, cleartext: true
+  use Ace.HTTP.Service, [cleartext: true]
+
+  def handle_request(
+    %{method: :GET, path: []},
+    _state)
+  do
+    response(:ok)
+    |> set_header("content-type", "text/plain")
+    |> set_body("Hello, World!")
+  end
+
+  def handle_request(
+    %{method: :GET, path: ["name", name]},
+    _state)
+  do
+    response(:ok)
+    |> set_header("content-type", "text/plain")
+    |> set_body("Hello, #{name}!")
+  end
 
   def handle_request(_request, _state) do
-    response(:ok)
+    response(:not_found)
     |> set_header("content-type", "text-plain")
-    |> set_body("Hello, World!")
+    |> set_body("Sorry, nothing here.")
   end
 end
