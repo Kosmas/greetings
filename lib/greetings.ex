9f3e09f0521c9847af6e002cf877279a472d27f1
Kosmas Chatzimichalis <kosmas@mach7x.com>
2018-02-18 13:27:59 +0100

    Initial commit

---
+++ lib/greetings.ex
@@ -0,0 +1,18 @@
+defmodule Greetings do
+  @moduledoc """
+  Documentation for Greetings.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Greetings.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
